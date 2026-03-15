f42f67496381a006905c279d1d5701087afc628f
2022bcs0008-nivithanoble <nivitha22bcs8@iiitkottayam.ac.in>
2026-03-15 07:07:52 +0530

    fixed compare_metrics

--- compare_metrics.py
+++ compare_metrics.py
@@ -3,7 +3,7 @@ import json
 with open("outputs/metrics/results.json") as f:
     metrics = json.load(f)
 
-if metrics["accuracy"] > 0.0:
+if metrics["R2"] > 0.0 or metrics["MSE"] < 4e9:
     print("Model improved")
 else:
     raise Exception("Model performance not good")
